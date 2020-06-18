fb2ebce0d641a7105a8e91a69eb97b82d2a4de91
can <onalcan20@gmail.com>
2020-06-18 14:35:31 +0300

    initial commit

---
+++ app/src/main/java/com/canonal/tictactoe/activity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.canonal.tictactoe.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.canonal.tictactoe.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_start)
+    TextView tvStart;
+    @BindView(R.id.tv_about)
+    TextView tvAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_start)
+    public void onTvStartClicked() {
+        startActivity(new Intent(MainActivity.this,GameActivity.class));
+    }
+
+    @OnClick(R.id.tv_about)
+    public void onTvAboutClicked() {
+    }
+}
